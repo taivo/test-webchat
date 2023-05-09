54ea804d99968bbe596b2845fad865cb82d1bf41
taivo <tai@localhost>
2023-05-08 21:34:11 -0700

    encapsulate with BotChat component

---
+++ src/components/BotChat.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import {useMemo} from 'react'
+import ReactWebChat, {createDirectLine} from 'botframework-webchat'
+
+export default function BotChat() {
+   // NOTE: to get token directly from our site, see this
+  // https://learn.microsoft.com/en-us/azure/bot-service/bot-service-channel-directline-extension-webchat-client?view=azure-bot-service-4.0
+  
+  const directLineToken = process.env.NEXT_PUBLIC_DIRECTLINE_TOKEN
+
+   const directLine = useMemo(async () => {
+   return createDirectLine({
+      token: directLineToken
+    })
+  }, [directLineToken])
+
+  if(!directLineToken){
+   return <div>Directline token not set</div>
+  }
+
+ return <ReactWebChat directLine={directLine} />
+}
